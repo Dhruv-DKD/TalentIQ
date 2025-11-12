eb4cdb5cca06ee3801555bed5e0416c2d6252621
DHRUV DADHANIA <dhruvdadhania9@gmail.com>
2025-11-12 16:16:50 +0530

    inngest app name correction

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/User.js";
 
-export const inngest = new Inngest({ id: "dhruv-dkd-talent-iq" });
+export const inngest = new Inngest({ id: "Dhruv-DKD-talent-IQ" });
 
 const syncUser = inngest.createFunction(
   {
