04d6634d208cb31dff0d6303a80f97df3b0e593c
DHRUV DADHANIA <dhruvdadhania9@gmail.com>
2025-11-12 09:57:51 +0530

    deployment added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Success from API" });
@@ -10,5 +13,18 @@ app.get("/health", (req, res) => {
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  const staticPath = path.join(__dirname, "../frontend/dist");
+  console.log("🧭 Static path:", staticPath);
+
+  app.use(express.static(staticPath));
+
+  // React Router fallback
+  // ✅ Works in Express 5+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(staticPath, "index.html"));
+  });
+}
 
 export default app;
