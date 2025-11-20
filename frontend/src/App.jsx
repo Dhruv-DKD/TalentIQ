a915c72fae73bbc8d62e5bafecedcac330a75ccd
DHRUV DADHANIA <dhruvdadhania9@gmail.com>
2025-11-20 08:53:34 +0530

    Home page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,32 @@ import { Toaster } from "react-hot-toast";
 
 import HomePage from "./pages/HomePage.jsx";
 import ProblemsPage from "./pages/ProblemsPage.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
 
 const App = () => {
   const { isSignedIn } = useUser();
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
         <Route
           path="/problems"
           element={isSignedIn ? <ProblemsPage /> : <Navigate to="/" />}
         />
       </Routes>
-<figure></figure>
       <Toaster toastOptions={{ duration: 3000 }} />
     </>
   );
 };
 
 // tailwind, daisyUI , react-router, react-hot-toast
-// todo : react-query aka tanstack query , axios 
+// todo : react-query aka tanstack query , axios
 
 export default App;
